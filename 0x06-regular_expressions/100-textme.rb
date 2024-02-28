dfb126b8e50c5c91322f9b688f37e97d46443b74
helmogey <h.elmogey@gmail.com>
2024-02-28 10:10:53 +0200

    6th

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # Ruby script that accepts one argument and pass it to a regular expression matching method
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join
+puts ARGV[0].scan(/\[(.*?)\], \[(.*?)\], \[(.*?)\]/).join
