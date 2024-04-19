16e1922808fe8775452e3f070b3a04cb1e001291
helmogey <h.elmogey@gmail.com>
2024-04-19 14:22:31 +0200

    fst

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,11 @@
 #!/usr/bin/pup
 # Using Puppet, install flask from pip3.
 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+class { 'python3::pip': }
+
+package { 'python3-flask':
+  ensure => '2.1.0',
+  provider => 'pip3',
+  require => Class['python3::pip'],
 }
 
